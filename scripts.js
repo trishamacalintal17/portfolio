18247ada0708c784b8e6c5603d9df3903011b35a
Trisha Macalintal <22-30990@g.batstate-u.edu.ph>
2024-10-23 20:55:18 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+let section = document.querySelectorAll('.section');
+let navLinks = document.querySelectorAll('.header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = window.offsetTop - 150;
+        let height = window.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+
+
+
